00a11529d4196cca75332a64c76a9a7c24d9ef3a
enthalpy-yan <enthalpy.yan@gmail.com>
2014-03-26 07:21:23 -0400

    change to use iterable instead of returning large list

--- imagefileinfo.py
+++ imagefileinfo.py
@@ -25,10 +25,10 @@ class JPGFileInfo(FileInfo):
     def __parse_attribute(self, filename):
         return re.compile(r'(.*)_.*').match(filename).group(1)
 
-def image_list(directory):
+def images(directory):
     """
-    Get a list that contains instances of JPGFileInfo class created by
-    all of files inside the input directory.
+    Get a iterable object that contains instances of JPGFileInfo
+    class created by all of files inside the input directory.
 
     Parameters
     ----------
@@ -38,12 +38,12 @@ def image_list(directory):
     -------
     A list of JPGFileInfo instances.
     """
-    fileList = [os.path.join(os.path.abspath(directory), f)
-                for f in os.listdir(directory)]
+    imagefiles = (os.path.join(os.path.abspath(directory), f)
+                  for f in os.listdir(directory))
     def get_class(filename, module=sys.modules[__name__]):
         "get file info class from filename extension"
         subclass = "%sFileInfo" % os.path.splitext(filename)[1].upper()[1:]
         return hasattr(module, subclass) and \
-                getattr(module, subclass) or FileInfo
-    return [get_class(os.path.basename(f))(os.path.basename(f), f)
-            for f in fileList]
+               getattr(module, subclass) or FileInfo
+    return (get_class(os.path.basename(f))(os.path.basename(f), f)
+            for f in imagefiles)
