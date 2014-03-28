eed95919697cc2b6cf9e5f6643a783f872b80d1b
minhui <guminhui@hotmail.com>
2014-03-27 20:18:01 -0400

    modified:   sample_generator.py

--- sample_generator.py
+++ sample_generator.py
@@ -4,8 +4,8 @@ import vq
 import time
 
 class Time_recorder():
-    def __init__(self,log_path):
-        self.f = open(log_path , 'w')
+    def __init__(self,log_path,permissions):
+        self.f = open(log_path ,permissions)
         
     def logging(self,content):
         self.f.write(content)
@@ -26,21 +26,37 @@ class SampleGenerator():
             if not os.path.exists(a): 
                 os.makedirs(a)
           
-    def __init__(self,category,K,img_folder,codebook_path,sample_path,pathfile,log_path):
-        self.__check_output_folders__(codebook_path,sample_path,pathfile,img_folder,log_path)
-        self.log_filename = log_path + category + ".log"
-        self.timerecorder = Time_recorder(self.log_filename)
+    def __init__(self,category,K,img_folder,codebook_path,sample_path,pathfile,log_path,multi_categories = False,index = 0):
+        
+        self.__check_output_folders__(codebook_path,sample_path,pathfile,img_folder,log_path)    
+        self.index = index
         self.category = category
+        self.multi_categories = multi_categories
         self.K = K
         self.psampels = []
         self.nsampels = []
         self.positive_sample_number = 0
         self.negative_sample_number = 0
-        self.samples_filename = sample_path + category + "_samples.txt"
         self.codebook = []
-        self.codebook_filename = codebook_path + category + "_codebook.txt"
         self.pathbook = {}
-        self.pathbook_filename = pathfile +category + "_path.txt"
+        self.timerecorder, self.log_filename, self.samples_filename,\
+            self.codebook_filename, self.pathbook_filename = "", "", "", "", ""
+        
+        if not multi_categories:
+            self.log_filename = log_path + category + ".log"
+            self.samples_filename = sample_path + category + "_samples.txt"
+            self.codebook_filename = codebook_path + category + "_codebook.txt"   
+            self.pathbook_filename = pathfile +category + "_path.txt"
+            self.timerecorder = Time_recorder(self.log_filename,'w')  
+        else:
+            print 
+            
+            self.log_filename = log_path + "Multiclass.log"
+            self.samples_filename = sample_path + "Multiclass_samples.txt"
+            self.codebook_filename = codebook_path +  "Multi_class_codebook.txt"   
+            self.pathbook_filename = pathfile + "Multi_class_path.txt"
+            self.timerecorder = Time_recorder(self.log_filename,'a')  
+        
         now = time.time()
         self.image_pool = fi.filter_by_attrs(fi.images_iter(img_folder))
         interval = time.time() - now
@@ -52,7 +68,10 @@ class SampleGenerator():
     def load_paths(self):
         self.pathbook = self.image_pool[self.category]
         print self.pathbook
-        f = open(self.pathbook_filename , 'w')
+        if not self.multi_categories:
+            f = open(self.pathbook_filename , 'w')
+        else:
+            f = open(self.pathbook_filename , 'a')
         for eachline in self.pathbook:
             f.write(eachline+'\n')
         f.close()
@@ -69,14 +88,17 @@ class SampleGenerator():
         self.psampels = vq.batch_quantization(self.pathbook[:counter], self.codebook,soft)
         interval = time.time() - now
         self.timerecorder.logging("%s :Generated %d psamples: %s seconds.\n"%(time.ctime(),num,interval))
-        f = open(self.samples_filename, 'w')
+        if not self.multi_categories:
+            f = open(self.samples_filename, 'w')
+        else:
+            f = open(self.samples_filename, 'a')
         i = 0
         now = time.time()
         for each_sample in self.psampels:
             if i < counter :
-                line = "+1 "
+                line = "+1 " if not self.multi_categories else "%d "%(self.index+1)
                 for k,v in each_sample.items():
-                    line = line +  "%s:%s "%(k,v)
+                    line = line +  "%s:%s "%(k+1,v)
                 f.write(line+'\n')
                 print line  
             i += 1
@@ -98,7 +120,7 @@ class SampleGenerator():
                 self.nsampels.append(hist)
                 line = "-1 "
                 for k,v in hist.items():
-                    line = line +  "%s:%s "%(k,v)
+                    line = line +  "%s:%s "%(k+1,v)
                 f.write(line+'\n')
                 print line
                 i += 1
